9a71567fe9b73a16bb3ef34124fd6b11450c6dcf
bjh0499 <j9603h@gmail.com>
2024-11-10 23:01:19 +0900

    testing to other URI

--- cnn.js
+++
@@ -1,1 +0,0 @@
-document.querySelector("#cbox_module").remove()
--- cnn/cnn.js
+++ cnn/cnn.js
@@ -5,11 +5,27 @@
   window.hasRun = true;
 
   function cnn() {
-    document.querySelector("#cbox_module").style.visibility = "hidden";
+    const URI = document.documentURI;
+    if (
+      URI.indexOf("n.news.naver.com") >= 0 ||
+      URI.indexOf("comic.naver.com/webtoon/detail") >= 0
+    ) {
+      document.querySelector("#cbox_module").style.visibility = "hidden";
+    } else if (URI.indexOf("mozilla.org") >= 0) {
+      document.querySelector("#outer-wrapper").style.visibility = "hidden";
+    }
   }
 
   function reset() {
-    document.querySelector("#cbox_module").style.visibility = "visible";
+    const URI = document.documentURI;
+    if (
+      URI.indexOf("n.news.naver.com") >= 0 ||
+      URI.indexOf("comic.naver.com/webtoon/detail") >= 0
+    ) {
+      document.querySelector("#cbox_module").style.visibility = "visible";
+    } else if (URI.indexOf("mozilla.org") >= 0) {
+      document.querySelector("#outer-wrapper").style.visibility = "visible";
+    }
   }
 
   browser.runtime.onMessage.addListener((message) => {
