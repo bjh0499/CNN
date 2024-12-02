37b24232dc7ecd78df9f1adeca4d98f89b1e8261
bjh0499 <j9603h@gmail.com>
2024-12-02 17:56:41 +0900

    fix bug: due to useCapture property placed to wrong function, it could not blocking whole click event

--- cnn/messageListener.js
+++ cnn/messageListener.js
@@ -3,11 +3,21 @@
     return;
   }
 
+  function test2(ev) {
+    ev.stopImmediatePropagation();
+    ev.preventDefault();
+    document.querySelector("body").removeEventListener("click", test2);
+  }
+
+  function test() {
+    document.querySelector("body").addEventListener("click", test2, true);
+  }
+
   window.hasRun = true;
 
   browser.runtime.onMessage.addListener((message) => {
     if (message.command === "test") {
-      console.log("TEST");
+      test();
     }
   });
 })();
