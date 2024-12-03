2a64a8c5809b0a590226389b165dcf2411c0e493
bjh0499 <j9603h@gmail.com>
2024-12-03 20:48:13 +0900

    attempt to create inspector function (some bugs occurred)

--- cnn/messageListener.js
+++ cnn/messageListener.js
@@ -4,13 +4,20 @@
   }
 
   const body = document.querySelector("body");
+  let tmp;
 
   function test2(ev) {
     ev.stopImmediatePropagation();
     ev.preventDefault();
+    console.log(tmp);
 
     body.removeEventListener("click", test2);
     body.removeEventListener("mouseover", test3);
+
+    const prevCnnSelector = document.querySelector("#cnnSelector");
+    if (prevCnnSelector) {
+      prevCnnSelector.remove();
+    }
   }
 
   function test3(ev) {
@@ -23,7 +30,25 @@
       prevCnnSelector.remove();
     }
 
+    const rect = ev.target.getBoundingClientRect();
     console.log(ev.target);
+    console.log(rect);
+    tmp = ev.target;
+
+    const cnnSelector = document.createElement("div");
+    cnnSelector.id = "cnnSelector";
+    cnnSelector.style.position = "fixed";
+    cnnSelector.style.width = `${rect.width}px`;
+    cnnSelector.style.height = `${rect.height}px`;
+    cnnSelector.style.left = `${rect.left}px`;
+    cnnSelector.style.top = `${rect.top}px`;
+    cnnSelector.style.zIndex = "100";
+    cnnSelector.style.backgroundColor = "red";
+    body.append(cnnSelector);
+
+    setTimeout(() => {
+      cnnSelector.remove();
+    }, 100);
   }
 
   function test() {
